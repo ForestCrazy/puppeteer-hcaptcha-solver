dd46804c6e3a255fe8c50d3b74007afc3efc99c1
ForestCrazy <65145713+ForestCrazy@users.noreply.github.com>
2022-10-19 09:05:02 +0700

    windows support
    * use await instant of setTimeout()
    * formatting code

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,18 @@
  * @author Shahzain Masood
  * Github: https://github.com/shahzain345/puppeteer-hcaptcha-solver
  */
-import { Page, Frame, Browser } from 'puppeteer';
-import { createCursor } from 'ghost-cursor-frames';
-import { get_result } from './python/get_result';
-import { install_py_files } from './installation';
+import { Page, Frame, Browser } from "puppeteer";
+import { createCursor } from "ghost-cursor-frames";
+import { get_result } from "./python/get_result";
+import { install_py_files } from "./installation";
 const sleep = (seconds: number) => {
   return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
-}
+};
 export class PuppeterHcaptchaSolve {
-  browser: Browser
-  use_gc: boolean
+  browser: Browser;
+  use_gc: boolean;
   constructor(browser: Browser, use_gc: boolean) {
-    this.browser = browser
+    this.browser = browser;
     this.use_gc = use_gc;
   }
   async solve(page: Page) {
@@ -21,45 +21,57 @@ export class PuppeterHcaptchaSolve {
       const isElmPresent = await this._detect_captcha(page);
       let cursor: any = null;
       if (isElmPresent) {
-        await page.click('iframe');
-        const frame = await page.frames().find(x => x.url().includes("https://newassets.hcaptcha.com"));;
+        await page.click("iframe");
+        const frame = await page
+          .frames()
+          .find((x) => x.url().includes("https://newassets.hcaptcha.com"));
         if (frame !== null && frame !== undefined) {
-          await frame.waitForSelector('.prompt-text').catch(async (e) => {
-            const token = await page.evaluate("hcaptcha.getResponse();")
+          await frame.waitForSelector(".prompt-text").catch(async (e) => {
+            const token = await page.evaluate("hcaptcha.getResponse();");
             if (token !== "") {
-              resolve(token)
+              resolve(token);
             } else {
-              resolve("Failed")
+              resolve("Failed");
             }
           });
 
           await frame.click(".language-selector");
-          const [btnLang] = await frame.$x("//span[contains(text(), 'English')]");
+          const [btnLang] = await frame.$x(
+            "//span[contains(text(), 'English')]"
+          );
           if (btnLang) {
-            await btnLang.click().catch(e => console.log("Failed to translate text"))
-
+            await btnLang
+              .click()
+              .catch((e) => console.log("Failed to translate text"));
           }
-          const elm = await frame.$('.prompt-text');
-          const _challenge_question = await frame.evaluate(el => el.textContent, elm);
+          const elm = await frame.$(".prompt-text");
+          const _challenge_question = await frame.evaluate(
+            (el) => el.textContent,
+            elm
+          );
           if (this.use_gc) {
             cursor = await createCursor(page);
           }
-          const token = await this._click_good_images(frame, _challenge_question, page, cursor);
-          resolve(token)
+          const token = await this._click_good_images(
+            frame,
+            _challenge_question,
+            page,
+            cursor
+          );
+          resolve(token);
         }
       } else {
-        throw Error('Captcha not detected')
+        throw Error("Captcha not detected");
       }
-    })
-
+    });
   }
 
   private async _detect_captcha(page: Page) {
     try {
-      await page.waitForSelector('iframe')
-      return true
+      await page.waitForSelector("iframe");
+      return true;
     } catch (e) {
-      return false
+      return false;
     }
   }
 
@@ -71,21 +83,50 @@ export class PuppeterHcaptchaSolve {
     }
   }
 
-  private async _click_good_images(frame: Frame, label: string, page: Page, cursor: any): Promise<string | undefined> {
+  private async _click_good_images(
+    frame: Frame,
+    label: string,
+    page: Page,
+    cursor: any
+  ): Promise<string | undefined> {
     return new Promise(async (resolve) => {
       await sleep(0.5);
       for (let i = 0; i < 9; i++) {
         await frame.waitForSelector(`div.task-image:nth-child(${i + 1})`, {
-          visible: true
-        })
-        await frame.waitForSelector(`div.task-image:nth-child(${i + 1}) > div:nth-child(2) > div:nth-child(1)`, {
-          visible: true
-        })
-        await frame.waitForFunction((i) => document.querySelector(`div.task-image:nth-child(${i + 1}) > div:nth-child(2) > div:nth-child(1)`)?.getAttribute("style")?.indexOf("url(") !== -1, {}, i)
-        const elementHandle = await frame.$(`div.task-image:nth-child(${i + 1}) > div:nth-child(2) > div:nth-child(1)`)
-        const style: string = await frame.evaluate((el) => el.getAttribute('style'), elementHandle)
-        const url = await style.split('url("')[1].split('")')[0]
-        const res = await get_result(url, label)
+          visible: true,
+        });
+        await frame.waitForSelector(
+          `div.task-image:nth-child(${
+            i + 1
+          }) > div:nth-child(2) > div:nth-child(1)`,
+          {
+            visible: true,
+          }
+        );
+        await frame.waitForFunction(
+          (i) =>
+            document
+              .querySelector(
+                `div.task-image:nth-child(${
+                  i + 1
+                }) > div:nth-child(2) > div:nth-child(1)`
+              )
+              ?.getAttribute("style")
+              ?.indexOf("url(") !== -1,
+          {},
+          i
+        );
+        const elementHandle = await frame.$(
+          `div.task-image:nth-child(${
+            i + 1
+          }) > div:nth-child(2) > div:nth-child(1)`
+        );
+        const style: string = await frame.evaluate(
+          (el) => el.getAttribute("style"),
+          elementHandle
+        );
+        const url = await style.split('url("')[1].split('")')[0];
+        const res = await get_result(url, label);
         if (res) {
           if (cursor !== null) {
             await cursor.click(`div.task-image:nth-child(${i + 1})`, {}, frame);
@@ -97,14 +138,12 @@ export class PuppeterHcaptchaSolve {
           await this._click_submit(frame, cursor);
           await sleep(1.5);
           const token = await page.evaluate("hcaptcha.getResponse();");
-          resolve(token)
-
+          resolve(token);
         }
       }
-    })
+    });
   }
-
 }
 (async () => {
   await install_py_files();
-})()
+})();
--- src/installation/index.ts
+++ src/installation/index.ts
@@ -1,31 +1,41 @@
 import axios from "axios";
-import fs from 'fs';
-import { exec } from 'child_process';
-import os from 'os';
+import fs from "fs";
+import { exec } from "child_process";
+import os from "os";
 export const install_py_files = async () => {
-    if (!fs.existsSync("./recognition/_main.py")) {
-        exec("mkdir recognition")
-        exec("touch ./recognition/_main.py")
-        setTimeout(async () => {
-            const { data } = await axios.get("https://raw.githubusercontent.com/shahzain345/puppeteer-hcaptcha-solver/main/recognition/_main.py");
-            fs.writeFile("./recognition/_main.py", data, (err) => {
-                if (err) throw err;
-            })
-            console.log(`Installed _main.py file...`)
-            console.log(`Installed all files...`)
-            if (os.type() == "Darwin") {
-                exec("python3 --version", (err, stdout) => {
-                    if (err) throw Error("Python3 not found. Please install python to use this package")
-                })
-            } else {
-                exec("python --version", (err, stdout) => {
-                    if (err) throw Error("Python3 not found. Please install python to use this package")
-                })
-            }
-            exec('pip install opencv-python requests httpx flask hcaptcha-challenger') // Install all the required python modules.
-            return true;
-        }, 4000)
-    } else {
-        return true;
-    }
-}
+  if (!fs.existsSync("./recognition/_main.py")) {
+    await exec("mkdir recognition");
+    console.log('create directory recognition');
+    await (async () => {
+      const { data } = await axios.get(
+        "https://raw.githubusercontent.com/shahzain345/puppeteer-hcaptcha-solver/main/recognition/_main.py"
+      );
+      fs.writeFile("./recognition/_main.py", data, (err) => {
+        if (err) throw err;
+      });
+      console.log(`Installed _main.py file...`);
+      console.log(`Installed all files...`);
+      if (os.type() == "Darwin") {
+        exec("python3 --version", (err, stdout) => {
+          if (err)
+            throw Error(
+              "Python3 not found. Please install python to use this package"
+            );
+        });
+      } else {
+        exec("python --version", (err, stdout) => {
+          if (err)
+            throw Error(
+              "Python3 not found. Please install python to use this package"
+            );
+        });
+      }
+      exec(
+        "pip install opencv-python requests httpx flask hcaptcha-challenger --user"
+      ); // Install all the required python modules.
+      return true;
+    })();
+  } else {
+    return true;
+  }
+};
